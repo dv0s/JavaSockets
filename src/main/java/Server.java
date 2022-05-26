8a17588178b1c905e6385356330667f9130f1fe3
Danny van Os <dannyvanos93@gmail.com>
2022-05-26 22:11:39 +0200

    Multiple clients can be active now
    One hugh bug though., they can both upload the same file on the same time. This should not be possible as it is effectively a collision

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -28,71 +28,9 @@ public class Server {
                 // Start een thread waar een client op aanhaakt.
                 new ServerThread(serverSocket.accept(), conn_count).start();
             }
-/*
-            // Open een oneindige lus voor het versturen van het bestand zodra er een client verbinding heeft gemaakt.
-            while (true) {
-                System.out.println("Client Accepted");
-
-                // Pak het bestand die je wilt versturen.
-                File myFile = new File("D:\\Avans\\Socket\\send\\BIGASSFILE.zip");
-
-                // Maak een teller voor straks
-                int count;
-                // Definieer een buffer straks
-                byte[] buffer = new byte[16 * 1024];
-
-                int i = 0;
-
-                // Hiermee kan de grootte van het bestand worden bepaald (Hebben we voor nu niet nodig).
-//                byte[] myByteArray = new byte[(int) myFile.length()];
-
-                // Zet een stream op waar we naartoe kunnen schrijven (Output gaat naar de andere kant toe).
-                OutputStream out = clientSocket.getOutputStream();
-
-                // Lees het bestand uit in een gebufferde stream (Input krijgt van de andere kant, in dit geval van het
-                // bestand wat eerder aangemaakt is).
-                BufferedInputStream in = new BufferedInputStream(new FileInputStream(myFile));
-
-                // Hier wordt het interessant, we gaan lussen zolang dat wat we krijgen van het bestand niet groter
-                // of gelijk is aan 0.
-                while((count = in.read(buffer)) >= 0){
-                    System.out.println(i + ": " + count);
-                    // Schrijf het buffer stukje naar de client stream.
-                    out.write(buffer, 0, count);
-                    // En wel direct
-                    out.flush();
-                    i++;
-                }
-
-                // Als we klaar zijn, sluiten we de connectie met de client.
-                clientSocket.close();
-
-//                System.out.println("Read length: " + myFile.length());
-//                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myFile));
-//
-//                System.out.println("Initiated BufferedInputStream");
-//                bis.read(myByteArray, 0, myByteArray.length);
-//
-//                System.out.println("Read the file");
-//                OutputStream os = clientSocket.getOutputStream();
-//
-//                System.out.println("Get output stream and write to stream");
-//                os.write(myByteArray, 0, myByteArray.length);
-//
-//                System.out.println("Flush it");
-//                os.flush();
-//
-//                System.out.println("And close");
-//                os.close();
-//
-//                clientSocket.close();
-
-                // Breek uit de lus.
-                break;
-            }
 
             System.out.println("Transfer should be completed.");
-*/
+
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
                     + portNumber + " or listening for a connection");
--- src/main/java/ServerThread.java
+++ src/main/java/ServerThread.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class ServerThread extends Thread{
@@ -32,12 +29,67 @@ public class ServerThread extends Thread{
             while(true){
                 System.out.println("Client "+ conn_count +" connectie is actief voor: " + System.nanoTime());
                 Thread.sleep(10000);
+                boolean done = TransferFile();
+                if(done){
+                    break;
+                }
             }
+
+            System.out.println("File has been transferred!");
+
 
         }catch(IOException e){
             e.printStackTrace();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    // De code van Server naar ServerThread verplaatst en in een methode gezet.
+    public boolean TransferFile() throws IOException {
+
+        // Open een oneindige lus voor het versturen van het bestand zodra er een client verbinding heeft gemaakt.
+        while (true) {
+            System.out.println("Client Accepted");
+
+            // Pak het bestand die je wilt versturen.
+            File myFile = new File("D:\\Avans\\Socket\\send\\BIGASSFILE.zip");
+
+            // Maak een teller voor straks
+            int count;
+            // Definieer een buffer straks
+            byte[] buffer = new byte[16 * 1024];
+
+            int i = 0;
+
+            // Hiermee kan de grootte van het bestand worden bepaald (Hebben we voor nu niet nodig).
+//                byte[] myByteArray = new byte[(int) myFile.length()];
+
+            // Zet een stream op waar we naartoe kunnen schrijven (Output gaat naar de andere kant toe).
+            OutputStream out = socket.getOutputStream();
+
+            // Lees het bestand uit in een gebufferde stream (Input krijgt van de andere kant, in dit geval van het
+            // bestand wat eerder aangemaakt is).
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(myFile));
+
+            // Hier wordt het interessant, we gaan lussen zolang dat wat we krijgen van het bestand niet groter
+            // of gelijk is aan 0.
+            while((count = in.read(buffer)) >= 0){
+                System.out.println(i + ": " + count);
+                // Schrijf het buffer stukje naar de client stream.
+                out.write(buffer, 0, count);
+                // En wel direct
+                out.flush();
+                i++;
+            }
+
+            // Als we klaar zijn, sluiten we de connectie met de client.
+            socket.close();
+
+            // Breek uit de lus.
+            break;
+        }
+
+        return true;
     }
 }
