5d8d0c24c93fcb675ea02e1e7840d9f5cd64dfc7
Danny van Os <dannyvanos93@gmail.com>
2023-03-02 23:31:56 +0100

    WIP: Created server for protocol implementation

--- src/main/java/Protocol/Protocol.java
+++ src/main/java/Protocol/Protocol.java
@@ -3,9 +3,10 @@ package Protocol;
 public class Protocol {
 
     public String processInput(String input){
-
         String output = null;
 
         return input;
     }
+
+    private void Stop(){}
 }
