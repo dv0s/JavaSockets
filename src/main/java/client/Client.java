8e19300fd9e4355a5b133c74afc38bc767164c81
Danny van Os <dannyvanos93@gmail.com>
2023-03-04 17:20:17 +0100

    Clean up

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -3,10 +3,6 @@ package client;
 import client.handlers.Connection;
 
 import java.io.*;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] args) throws IOException {
@@ -58,60 +54,21 @@ public class Client {
                 break;
             }
 
-            System.out.print("Command: ");
-            // Onze blokkende command afwacht ding
-            fromUser = stdIn.readLine();
+            // Er moet een manier worden gevonden om te weten wanneer de client mag praten.
+            // if(fromServer.equals("") && previous != null){
 
-            if (fromUser != null) {
-                System.out.println("Client: " + fromUser);
+                System.out.print("Command: ");
+                // Onze blokkende command afwacht ding
+                fromUser = stdIn.readLine();
 
-                // Stuur de input door naar de Server.
-                connection.serverOut.println(fromUser);
-            }
+                if (fromUser != null) {
+                    System.out.println("Client: " + fromUser);
+
+                    // Stuur de input door naar de Server.
+                    connection.serverOut.println(fromUser);
+                }
+            //}
         }
-
-        //try(
-                // Probeer een socket op te zetten naar de server die open zou moeten staan.
-          //      Socket serverSocket = new Socket(hostName, portNumber)
-        //){
-
-
-           //FileOutputStream fos = new FileOutputStream("D:\\Avans\\Socket\\received\\big_ass_file.zip");
-
-            // Open een gebufferde stream voor het te ontvangen bestand (Welke we overigens weg kunnen laten).
-            //BufferedOutputStream out = new BufferedOutputStream(fos);
-
-            // Bepaal een buffer.
-          //  byte[] buffer = new byte[16 * 1024];
-            // Count variable voor de loop straks.
-//            int count;
-  //          int i = 0;
-
-            // Open de stream van de server waar de bytes van het bestand daalijk op binnen komen (Input krijgt van
-            // de andere kant).
-    //        InputStream in = serverSocket.getInputStream();
-
-            // Hier wordt het interessant, we gaan op buffergrootte lussen zolang als dat er bytes binnen komen.
-      //      System.out.println("Write to file");
-//            while((count = in.read(buffer)) >= 0){
-//                System.out.println(i + ": " + count);
-//                // Schrijf naar het bestand stream toe.
-//                fos.write(buffer, 0, count);
-//                // En wel direct.
-//                fos.flush();
-//                i++;
-//            }
-
-            // Als alles klaar is, dan sluiten we het bestand stream.
-        //    fos.close();
-
-      //  }catch(UnknownHostException e){
-      //      System.err.println("Don't know about host " + hostName);
-      //      System.exit(1);
-      //  }catch(IOException e){
-      //      System.err.println("Couldn't get I/O for the connection to " + hostName);
-      //      System.exit(1);
-        //}
 
     }
 }
