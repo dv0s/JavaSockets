662d8779f1d7699e8afecdfa2cee5cb10f61c258
Danny van Os <dannyvanos93@gmail.com>
2023-03-21 20:33:26 +0100

    WIP List directories

--- src/main/java/protocol/commands/List.java
+++ src/main/java/protocol/commands/List.java
@@ -1,44 +1,74 @@
 package protocol.commands;
 
+import protocol.enums.Command;
 import protocol.enums.Constants;
 import protocol.enums.Invoker;
 import protocol.interfaces.CommandHandler;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Array;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 
 public class List implements CommandHandler {
 
     public final Invoker invoker;
     public final Path homeDirectory;
-    public final BufferedReader clientIn;
-    public final PrintWriter clientOut;
+    public final BufferedReader in;
+    public final PrintWriter out;
     public final ArrayList<String> params;
 
-    public List(Invoker invoker, Path homeDirectory, BufferedReader clientIn, PrintWriter clientOut, ArrayList<String> params) {
+    public List(Invoker invoker, Path homeDirectory, BufferedReader in, PrintWriter out, ArrayList<String> params) {
         this.invoker = invoker;
         this.homeDirectory = homeDirectory;
-        this.clientIn = clientIn;
-        this.clientOut = clientOut;
+        this.in = in;
+        this.out = out;
         this.params = params;
     }
 
 
     @Override
-    public void handle(ArrayList<String> args) {
+    public void handle(ArrayList<String> args){
+        System.out.println(homeDirectory.toString());
 
-        clientOut.println(output());
-    }
+        // List commando moet ervoor zorgen dat de andere een lijst van bestanden teruggeeft die het in het bezit heeft.
+        // Server
 
-    public void handleClient(ArrayList<String> args){
+        if(invoker == Invoker.CLIENT){
+            out.println(Command.LS);
+        }else{
+            try {
+                Files.list(homeDirectory).forEach((file) -> {
 
-    }
+                    // Define variables
+                    BasicFileAttributes attributes;
+                    try {
+                        attributes = Files.readAttributes(file, BasicFileAttributes.class);
+                        out.println(
+                                file + "\u001f" +
+                                file.getFileName()+"\u001f"+
+                                        Files.size(file)+"\u001f"+
+                                        attributes.lastAccessTime()+"\u001f"+attributes.lastModifiedTime()
+                                        +"\u001c"
+                        );
 
-    public void handleServer(ArrayList<String> args){
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
 
+                });
+
+            } catch (IOException e) {
+                System.out.print(e.getMessage());
+            }
+        }
+
+        out.println(output());
     }
 
     @Override
