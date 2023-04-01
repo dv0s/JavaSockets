414bc42f1fdb35dfb102b2faa74ac17831afdb6c
niekvlot <vlotniek@gmail.com>
2023-04-01 20:00:03 +0200

    Cleanup FileWatcherHandler

--- src/main/java/client/handlers/FileWatcherHandler.java
+++ src/main/java/client/handlers/FileWatcherHandler.java
@@ -1,5 +1,6 @@
 package client.handlers;
 
+import protocol.commands.Delete;
 import protocol.commands.Put;
 import protocol.enums.Constants;
 import protocol.enums.Invoker;
@@ -7,6 +8,7 @@ import protocol.handlers.ConnectionHandler;
 
 import java.io.*;
 import java.nio.file.*;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -22,43 +24,25 @@ public class FileWatcherHandler implements Runnable {
     private static final ArrayList<File> deletedFiles = new ArrayList<>();
 
     public FileWatcherHandler(String[] args) {
-        try {
-            this.args = args;
-            scanner = new Scanner(System.in);
-            watchService = FileSystems.getDefault().newWatchService();
-            clientDir = Paths.get(Constants.BASE_DIR + File.separator + "client");
-
-            clientDir.register(watchService, ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE);
-
-            System.out.println("FileWatcher started...");
-            run();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        this.args = args;
+        scanner = new Scanner(System.in);
+        clientDir = Paths.get(Constants.BASE_DIR + File.separator + "client");
+        run();
     }
 
     @Override
     public void run() {
+        startFileWatcher();
+
         Thread monitor = new Thread(() -> {
-            try {
-                System.out.println(clientDir + " is in monitoring state...");
-                monitorChanges();
-            } catch (InterruptedException | IOException e) {
-                if (e instanceof InterruptedException) {
-                    if (!Thread.currentThread().isInterrupted()) {
-                        Thread.currentThread().interrupt();
-                    }
-                    System.out.println(clientDir + " is no longer in monitoring state...");
-                } else {
-                    throw new RuntimeException(e);
-                }
-            }
+            System.out.println(clientDir + " is in monitoring state...");
+            monitorChanges();
         });
 
         monitor.start();
 
         // User has sent data, so close the thread
-        if (scanner.nextLine() != null) {
+        if (scanner.nextLine().equalsIgnoreCase("CLOSE")) {
             monitor.interrupt();
         }
 
@@ -68,10 +52,10 @@ public class FileWatcherHandler implements Runnable {
                 connection = new ConnectionHandler(Invoker.CLIENT, clientDir).establish(args);
 
                 System.out.println("Sending new files to server...");
-                onPut();
+                putToServer();
 
                 System.out.println("Deleting files ...");
-                onDelete();
+                deleteOnServer();
 
                 // Close the server connection
                 connection.close();
@@ -81,43 +65,99 @@ public class FileWatcherHandler implements Runnable {
         }
     }
 
-    private static void monitorChanges() throws InterruptedException, IOException {
-        boolean poll = true;
+    private static void monitorChanges() {
+        try {
+            boolean poll = true;
 
-        while (poll) {
-            WatchKey key = watchService.take();
-            Path path = (Path) key.watchable();
+            while (poll) {
+                WatchKey key = watchService.take();
+                Path path = (Path) key.watchable();
 
-            for (WatchEvent<?> event : key.pollEvents()) {
-                File file = path.resolve((Path) event.context()).toFile();
-
-                if (event.kind() == ENTRY_CREATE || event.kind() == ENTRY_MODIFY) {
-                    changedFiles.add(file);
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    handleWatchEvent(event, path);
                 }
-                if (event.kind() == ENTRY_DELETE) {
-                    deletedFiles.add(file);
-                }
+
+                poll = key.reset();
             }
-
-            poll = key.reset();
+        } catch (InterruptedException | NoSuchAlgorithmException | IOException e) {
+            if (e instanceof InterruptedException) {
+                closeFileWatcher();
+                Thread.currentThread().interrupt();
+            } else {
+                throw new RuntimeException(e);
+            }
         }
-
-        watchService.close();
     }
 
-    private static void onPut() {
+    private static void handleWatchEvent(WatchEvent<?> event, Path path) throws NoSuchAlgorithmException, IOException {
+        File file = path.resolve((Path) event.context()).toFile();
+
+        if (event.kind() == ENTRY_CREATE || event.kind() == ENTRY_MODIFY) {
+            // Remove from deletedFiles array
+            deletedFiles.remove(file);
+
+            // Add only if not already exist.
+            if (!changedFiles.contains(file)) changedFiles.add(file);
+        }
+
+        if (event.kind() == ENTRY_DELETE) {
+            // Remove from changedFiles array
+            changedFiles.remove(file);
+
+            // Add only if not already exist.
+            if (!deletedFiles.contains(file)) deletedFiles.add(file);
+        }
+    }
+
+    private static void putToServer() {
+        // We are creating one object in memory
+        ArrayList<String> args = new ArrayList<>();
+
         for (File file : changedFiles) {
-            System.out.println("Send to server: " + file.getPath());
-            ArrayList<String> args = new ArrayList<>();
+            // Add file to the arraylist
             args.add(file.getName());
+
+            System.out.println("Send to server: " + file.getName());
             new Put(Invoker.CLIENT, clientDir, connection.socket, connection.in, connection.out).handle(args);
+
+            // Remove file from the arraylist
+            args.remove(0);
         }
     }
 
-    private static void onDelete() {
+    private static void deleteOnServer() {
+        // We are creating one object in memory
+        ArrayList<String> args = new ArrayList<>();
+
         for (File file : deletedFiles) {
-            System.out.println("Delete on server: " + file.getPath());
-            // new Delete(invoker, homeDirectory, socket, in, out).handle(params);
+            // Add file to the arraylist
+            args.add(file.getName());
+
+            System.out.println("Delete on server: " + file.getName());
+            //new Delete(Invoker.CLIENT, clientDir, connection.socket, connection.in, connection.out).handle(args);
+
+            // Remove file from the arraylist
+            args.remove(0);
+        }
+    }
+
+    private static void startFileWatcher() {
+        try {
+            watchService = FileSystems.getDefault().newWatchService();
+            clientDir.register(watchService, ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void closeFileWatcher() {
+        try {
+            if (watchService != null) {
+                watchService.close();
+                System.out.println("FileWatcher is no longer in monitor state...");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
